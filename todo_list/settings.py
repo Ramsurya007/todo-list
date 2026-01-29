6fba18892a8d6e9c562b7b48d0c590dea74ff3a5
Ramsurya <j.ramsurya007@gmail.com>
2026-01-29 10:53:54 +0530

    Fix ALLOWED_HOSTS for Render domain

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -27,11 +27,15 @@ SECRET_KEY = os.environ.get(
     "django-insecure-temp-key"
 )
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "False") == "True"
+DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get(
-    "ALLOWED_HOSTS", "127.0.0.1,localhost"
-).split(",")
+ALLOWED_HOSTS = [
+    "todo-list-uh1.onrender.com",
+    ".onrender.com",
+    "localhost",
+    "127.0.0.1",
+]
+
 
 # Application definition
 
